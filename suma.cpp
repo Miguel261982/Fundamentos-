642a6f4964642e46fce6bfefbecf7a4f6b1cbbfe
El Migue <miguel.bennett.valencia@utelvt.edu.ec.git config --global user.name El>
2022-09-22 21:37:50 -0500

    sume

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+//Creado por Miguel Bennettn
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+float x,y,z;
+cout<<"Ingrese 2 numero x y :";
+cin>>x>>y;
+z=x+y;
+cout<<"El resultado es : ";
+cout<<z<<endl;
+
+return 0;
+
+}
