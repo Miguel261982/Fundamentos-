e18cfc6b263753ebf410826352c32c529649db41
El Migue <miguel.bennett.valencia@utelvt.edu.ec.git config --global user.name El>
2022-09-24 21:23:26 -0500

    crear compara

---
+++ compara.cpp
@@ -0,0 +1,25 @@
+//Creado por: Miguel Angel Bennett Valencia
+#include<iostream>
+using namespace std; 
+int  main()
+{    
+	float M,B;
+	cout<<"Ingrese M B:"; cin>>M>>B;
+	if (M>B)
+	{
+	 cout<<M<<" es mayor M "<<B<<endl;
+
+	}else{ 
+
+	cout<<B<<"es mayor M "<<M<<endl;
+
+	}
+
+	return 0; 
+
+
+
+
+
+
+}
